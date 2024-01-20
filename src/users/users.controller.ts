5928f90ada9c2f480bc9b38ef240ab614158d09a
RattanakpanhaKONG <krattanakpanha@gmail.com>
2024-01-20 15:18:17 +0700

    Users Controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    /** 
+     * PATCH /user/:id
+     * DELETE /users/:id
+     */
+
+    @Get() //GET /users or /users?role=value
+    findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN'){
+        return []
+    }
+
+    @Get(':id') //GET /users/:id
+    findOne(@Param('id') id: string){
+        return { id }
+    }
+
+    @Post() //POST /users
+    create(@Body() user: {}){
+        return user
+    }
+
+    @Patch(':id') //PATCH /users/:id
+    update(@Param('id') id: string, @Body() userUpdate: {}){ //find user with id and update with the body
+        return { id, ...userUpdate }
+    }
+
+    @Delete(':id') //DELETE /users/:id
+    delete(@Param('id') id: string){
+        return { id }
+    }
+}
