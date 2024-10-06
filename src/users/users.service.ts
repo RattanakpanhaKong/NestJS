379a0f6fe6df725abeb38728773b90663596c5eb
RattanakpanhaKONG <krattanakpanha@gmail.com>
2024-10-06 13:48:09 +0700

    Updateed

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,57 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      role: 'ENGINEER',
+    },
+    {
+      id: 2,
+      name: 'Alice Smith',
+      email: 'alice.smith@example.com',
+      role: 'ADMIN',
+    },
+    {
+      id: 3,
+      name: 'Bob Johnson',
+      email: 'bob.johnson@example.com',
+      role: 'useINTERN',
+    },
+    {
+      id: 4,
+      name: 'Eva Brown',
+      email: 'eva.brown@example.com',
+      role: 'ENGINEER',
+    },
+    {
+      id: 5,
+      name: 'Charlie Wilson',
+      email: 'charlie.wilson@example.com',
+      role: 'INTERN',
+    },
+  ];
+
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+    return this.users;
+  }
+
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+    return user;
+  }
+
+  create(user: {
+    name: string;
+    email: string;
+    role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+  }){
+    const usersByHighestId = []
+  };
+}
